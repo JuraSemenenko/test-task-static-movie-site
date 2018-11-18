5a3bf586678c54c5d8393c9ea60e68efb59aba5c
JuraSemenenko <jura.semenenko@gmail.com>
2018-11-18 15:11:52 +0200

    Layout of movie page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ const START_URL =
   "https://api.themoviedb.org/3/discover/movie?api_key=340af08aad86d2a893fef0bc25ea615d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
 const API_KEY = "340af08aad86d2a893fef0bc25ea615d";
 const API_URL = "https://api.themoviedb.org/3/";
+const MONTH = [
+  "",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
 const MOVIE_ID =
   window.location.search !== ""
@@ -40,14 +55,12 @@ const fetchMoviesData = URL =>
     .then(data => makeMovieTamplate(data.results));
 
 const fetchMovieData = URL => {
-  fetch(URL, {
+  return fetch(URL, {
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json"
     }
-  })
-    .then(response => response.json())
-    .then(data => console.log(data));
+  }).then(response => response.json());
 };
 
 const makeMovieTamplate = data => {
@@ -79,10 +92,50 @@ const makeMovieTamplate = data => {
   movieContainer !== null ? (movieContainer.innerHTML = html) : null;
 };
 
+const fillPageWithData = data => {
+  const poster = document.getElementsByClassName("poster-img")[0];
+  const backdrop = document.getElementsByClassName("backdrop-img")[0];
+  const title = document.getElementsByClassName("title")[0];
+  const description = document.getElementsByClassName("description-text")[0];
+  const genres = document.getElementsByClassName("genres")[0];
+  const release = document.getElementsByClassName("release")[0];
+  const reating = document.getElementsByClassName("avg-reating")[0];
+  const releaseStatus = document.getElementsByClassName("release-status")[0];
+  const prodactionCountry = document.getElementsByClassName(
+    "prodaction-country"
+  )[0];
+  const slogan = document.getElementsByClassName("slogan")[0];
+
+  const avgRaiting = Math.ceil(+data.vote_average / 2);
+  const releaseDate = data.release_date.split("-");
+  const releaseDateTamplate = `${releaseDate[2]} ${MONTH[releaseDate[1]]} ${
+    releaseDate[1]
+  }`;
+  const prodactionCountryList = data.production_countries
+    .map(country => country.name)
+    .join(", ");
+  console.log(avgRaiting);
+  const genresList = data.genres.map(genre => genre.name).join(", ");
+
+  poster.src = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+  backdrop.src = `https://image.tmdb.org/t/p/original${data.backdrop_path}`;
+  title.innerHTML = `${data.original_title}`;
+  description.innerHTML = `${data.overview}`;
+  genres.innerHTML = genresList;
+  release.innerHTML = releaseDateTamplate;
+  reating.innerHTML = `${data.vote_average} / 10`;
+  releaseStatus.innerHTML = data.status;
+  prodactionCountry.innerHTML = prodactionCountryList;
+  slogan.innerHTML = data.tagline;
+};
+
 console.log(MOVIE_ID);
 if (MOVIE_ID !== undefined) {
   const URL = `https://api.themoviedb.org/3/movie/${MOVIE_ID}?api_key=340af08aad86d2a893fef0bc25ea615d&language=en-US`;
   console.log(URL);
-  fetchMovieData(URL);
+  fetchMovieData(URL).then(data => {
+    console.log(data);
+    fillPageWithData(data);
+  });
 }
 fetchMoviesData(START_URL);
